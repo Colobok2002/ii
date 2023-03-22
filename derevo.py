79daad3edeccf1c61c6989cfa18d7edc5ca07a88
Ilya <ilya.bv.2002@bk.ru>
2023-03-22 10:57:56 +0300

    1.5.1 Добавил структуру файлам

--- derevo.py
+++ derevo.py
@@ -4,50 +4,68 @@ from config import *
 from sklearn import tree
 
 
-base = read(target_file_name)
-data = dict()
-data[0], data[1] = [], []
+def mas_for_derevo():
 
-arrau_last_litel_table = []
-arrau_litel_table = []
-arrau_last_table = []
-arrau_table = []
-Shetsilk = 1
+    base = read(target_file_name)
+    data = dict()
+    data[0], data[1] = [], []
+    arrau_last_litel_table = []
+    arrau_litel_table = []
+    arrau_last_table = []
+    arrau_table = []
+    Shetsilk = 1
 
-for target in base:
+    for target in base:
 
-    if Shetsilk == 1 or Shetsilk == 2:
-        data[1].append(target)
-        arrau_last_table.append(target[-1])
-        arrau_table.append(target[:-1])
-        Shetsilk += 1
+        if Shetsilk == 1 or Shetsilk == 2:
+            data[1].append(target)
+            arrau_last_table.append(target[-1])
+            arrau_table.append(target[:-1])
+            Shetsilk += 1
 
-    else:
-        data[0].append(target)
-        arrau_last_litel_table.append(target[-1])
-        arrau_litel_table.append(target[:-1])
-        Shetsilk = 1
+        else:
+            data[0].append(target)
+            arrau_last_litel_table.append(target[-1])
+            arrau_litel_table.append(target[:-1])
+            Shetsilk = 1
 
-arr = []
+    arr = []
 
-for i in range(0, len(arrau_last_table)-2, 2):
-    arr.append([arrau_last_table[i], arrau_last_table[i+1]])
+    for i in range(0, len(arrau_last_table)-2, 2):
+        arr.append([arrau_last_table[i], arrau_last_table[i+1]])
 
-ed = 0
-nuli = 0
-clf = tree.DecisionTreeClassifier()
-big_table = clf.fit(arr, arrau_last_litel_table)
-y = clf.predict(arr)
+    return arr,arrau_last_litel_table
 
-for i in range(len(y)-1):
-    el1 = arrau_last_litel_table[i]
-    el2 = y[i]
-    if el1 == el2:
-        ed += 1
-    else:
-        nuli += 1
+def klasifier_metod():
 
-tree.plot_tree(clf)
+    arr,arrau_last_litel_table = mas_for_derevo()
+    ed = 0
+    nuli = 0
+    clf = tree.DecisionTreeClassifier()
+    clf.fit(arr, arrau_last_litel_table)
+    y = clf.predict(arr)
 
-print(ed/len(y))
+    for i in range(len(y)-1):
+        el1 = arrau_last_litel_table[i]
+        el2 = y[i]
+        if el1 == el2:
+            ed += 1
+        else:
+            nuli += 1
 
+    tree.plot_tree(clf)
+
+    print(ed/len(y))
+
+def regresion_metod():
+
+    arr, arrau_last_litel_table = mas_for_derevo()
+
+    clf = tree.DecisionTreeRegressor()
+    clf.fit(arr, arrau_last_litel_table)
+    y = clf.predict(arr)
+    print(y)
+
+if __name__ == '__main__':
+    klasifier_metod()
+    regresion_metod()
